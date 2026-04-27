0ddd0ec06c618a5444361fe13a32efd006534f14
victorbouissoumota <victorbouissoumota@gmail.com>
2026-04-27 18:22:44 -0300

    Questão 2 e 3

--- funcoes.py
+++ funcoes.py
@@ -7,3 +7,17 @@ def rolar_dados (n):
         lista.append(num)
         i += 1
     return lista
+
+def guardar_dado (dados_rolados, dados_noestoque, dados_paraguardar):
+    dado = dados_rolados[dados_paraguardar]
+    dados_noestoque.append(dado)
+    dados_rolados = dados_rolados[:dados_paraguardar] + dados_rolados [dados_paraguardar+1:]
+    return [dados_rolados, dados_noestoque]
+
+def remover_dado(dados_rolados, dados_no_estoque, dado_para_remover):
+    dado = dados_no_estoque[dado_para_remover]
+    dados_rolados.append(dado)
+    dados_no_estoque = dados_no_estoque[:dado_para_remover] + dados_no_estoque[dado_para_remover + 1:]
+    return [dados_rolados, dados_no_estoque]
+       
+
