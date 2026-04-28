924e325a3e16d9a1e43d46ca786051c38fb40fd7
victorbouissoumota <victorbouissoumota@gmail.com>
2026-04-28 15:52:53 -0300

    Questão 7

--- funcoes.py
+++ funcoes.py
@@ -45,5 +45,12 @@ def calcula_pontos_sequencia_baixa(dados):
         return 15
     return 0
 
+def calcula_pontos_sequencia_alta (dados):
+    if 1 in dados and 2 in dados and 3 in dados and 4 in dados and 5 in dados:
+        return 30
+    if 2 in dados and 3 in dados and 4 in dados and 5 in dados and 6 in dados:
+        return 30
+    return 0
+
        
 
